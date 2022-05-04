dd9e296cfd62ee0b0a7915456945e5a76dc654ae
uzregxurshid <uzregxurshid@gmail.com>
2022-05-05 03:56:39 +0500

    intro fixes

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,26 +2,27 @@ import React from 'react'
 import './intro.scss'
 import ArrowIcon from '../../assets/images/intro/arrow.svg';
 import Button from '../button/button';
+import Matras from '../../assets/images/intro/krovat.png';
 const Intro = () => {
   return (
     <div className='intro'>
       <div className="container">
         <div className="intro__container">
           <div className="intro__left">
-            <div className="intro__left__title">
+            <div className="intro__left--title">
               <h1 className='intro__header'>
                 Kechalari sokin dam oling
               </h1>
               <Button text={'Kategoriyalar'} img={ArrowIcon}/>
               <div className='intro__progress'>
-                  <span className='intro__pro'>
-                    
+                  <span className='intro__prog'>
+                    <span className='intro__prog--current'></span>
                   </span>
               </div>
             </div>
           </div>
           <div className="intro__right">
-
+              <img className='intro__matras' src={Matras} alt="matras" width={705} height={380}/>
           </div>
         </div>
       </div>
