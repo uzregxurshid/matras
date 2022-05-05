5a2ef81847a7cb2af42a208e10ab06efebf928e6
uzregxurshid <uzregxurshid@gmail.com>
2022-05-05 14:53:22 +0500

    fixed

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Header from '../../components/header/header'
-import Intro from '../../components/intro/intro.jsx'
-import Products from '../../components/products/products'
-import Stat from '../../components/stat/stat'
+import Header from '../../components/header/header';
+import Intro from '../../components/intro/intro';
+import Products from '../../components/products/products';
+import Stat from '../../components/stat/stat';
 const Home = () => {
   return (
     <>
